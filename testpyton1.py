4fc78e192833e1fe183066cb4c85340edb0236b5
Плетнева Наталья Владимировна <NPletneva@vimpelcom.ru>
2019-04-12 12:25:31 +0300

    my first commit

---
+++ testpyton1.py
@@ -0,0 +1,20 @@
+import pymysql.cursors
+
+# Подключиться к базе данных.
+connection = pymysql.connect(host='172.26.142.174',
+                             user='uric',
+                             password='uio987',
+                             db='nmcdb',
+                             charset='cp1251',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+print("connect successful!!")
+
+mycursor = connection.cursor()
+
+mycursor.execute("SELECT * FROM yes_no where id=1")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
